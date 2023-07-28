4031a5551c8388b369c55e0d85dcd243a3223406
Ze-hs <thehao33@gmail.com>
2023-07-28 01:10:50 -0400

    fixed logger functions

--- backend/utils/logger.js
+++ backend/utils/logger.js
@@ -1,9 +1,9 @@
-const info = (message) => {
-	console.log(message);
+const info = (...message) => {
+	console.log(...message);
 };
 
-const error = (error) => {
-	console.log(error);
+const error = (...error) => {
+	console.log(...error);
 };
 
 module.exports = { info, error };
